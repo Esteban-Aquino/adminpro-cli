dc5477612783ea20d2ea8bea7b4920aa90c22314
Mefistoel <eaquinof@gmail.com>
2019-10-12 20:05:29 -0500

    Maquetacion sin funcionalidad

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,35 @@
-import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+
+import { PagesComponent } from './pages/pages.component';
+
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { Graficas1Component } from './pages/graficas1/graficas1.component';
+import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
+import { RegisterComponent } from './login/register.component';
 
 
-const routes: Routes = [];
 
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
+
+const routes: Routes = [
+  { path: '',
+    component: PagesComponent,
+    children: [
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'progress', component: ProgressComponent },
+      { path: 'graficas1', component: Graficas1Component },
+      { path: '', redirectTo: '/dashboard', pathMatch: 'full' }
+    ] },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: '**', component: NopagefoundComponent }
+  // { path: 'path/:routeParam', component: MyComponent },
+  // { path: 'staticPath', component: ... },
+  // { path: '**', component: ... },
+  // { path: 'oldPath', redirectTo: '/staticPath' },
+  // { path: ..., component: ..., data: { message: 'Custom' }
+];
+
+export const APP_ROUTES = RouterModule.forRoot(routes, {useHash: true});
